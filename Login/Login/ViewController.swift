cf787e63fccb0d6b56b22b31c661aa8fe8feb34b
Cyberdev <cyberdev@cyberdevs-mbp.attlocal.net>
2018-12-06 21:07:41 -0600

    Login segue

--- Login/Login/ViewController.swift
+++ Login/Login/ViewController.swift
@@ -19,7 +19,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+        guard let sender = sender as? UIButton else {return}
+        if sender == FPbutton{
+            segue.destination.navigationItem.title = "forgot password"
+            
+        } else if sender == FUbutton {
+            segue.destination.navigationItem.title = "forgot username"
+        } else{    segue.destination.navigationItem.title = usernameBox.text
+            
+        }
+    }
 
-
+    @IBOutlet weak var usernameBox: UITextField!
+    
+    @IBOutlet weak var FUbutton: UIButton!
+    @IBAction func FUbutton(_ sender: Any) {
+    performSegue(withIdentifier: "VCsegue", sender: FUbutton)
+    }
+    
+    
+    @IBOutlet weak var FPbutton: UIButton!
+    @IBAction func FPbutton(_ sender: Any) {
+    performSegue(withIdentifier: "VCsegue", sender: FPbutton)
+    }
+    
+    
+    
 }
 
