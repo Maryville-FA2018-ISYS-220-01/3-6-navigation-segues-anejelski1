5a44b830befcbcb5c49313b696210a14f4bffe90
Cyberdev <cyberdev@cyberdevs-mbp.attlocal.net>
2018-12-06 20:29:32 -0600

    traffic segues

--- TrafficSegues/TrafficSegues/ViewController.swift
+++ TrafficSegues/TrafficSegues/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segueSwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
+
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func yellowB(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "yellow", sender: nil)
+        }
+    }
+    
+    @IBAction func greenB(_ sender: Any) {        if segueSwitch.isOn {
+        performSegue(withIdentifier: "green", sender: nil)
+        }
+    }
+    
+    
 }
 
